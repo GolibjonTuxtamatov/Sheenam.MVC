92be27189be1c0b4a2f1af09f02bd348fa611e76
99894 <99894@DESKTOP-K351R76>
2023-12-07 20:17:54 +0500

    DATA: User Model & Migrate

---
+++ Sheenam.MVC/Brokers/Storages/StorageBroker.Users.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sheenam.MVC.Models.Users;
+
+namespace Sheenam.MVC.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Sheenam.MVC/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,23 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sheenam.MVC.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source = ./Data/Sheenam.db";
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
+        public override void Dispose() { }
+    }
+}
