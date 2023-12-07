bf43cde1fabcd2d326c9b144fffd41e2bcd8414c
99894 <99894@DESKTOP-K351R76>
2023-12-07 20:41:59 +0500

    BROKERS: Insert User

---
+++ Sheenam.MVC/Brokers/Storages/IStorageBroker.Users.cs
@@ -0,0 +1,9 @@
+﻿using Sheenam.MVC.Models.Users;
+
+namespace Sheenam.MVC.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<User> InsertUserAsync(User user);
+    }
+}
