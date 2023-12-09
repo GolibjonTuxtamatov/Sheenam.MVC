8f9afe365d351c3236d598a3ef3adb4d08d292cb
99894 <99894@DESKTOP-K351R76>
2023-12-09 15:29:29 +0500

    ShouldThrowValidationExceptionIfUserIsInvalidAndLogItAsync - FAIL

--- Sheenam.MVC.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validation.cs
+++ Sheenam.MVC.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validation.cs
@@ -46,5 +46,66 @@ namespace Sheenam.MVC.Tests.Unit.Services.Foundations.Users
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionIfUserIsInvalidAndLogItAsync(string text)
+        {
+            //given
+            User invalidUser = new User
+            {
+                FirstName = text,
+            };
+
+            var invalidUserException = new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.FirstName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.LastName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.DateOfBirth),
+                values: "Date is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Email),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Password),
+                values: "Password is required");
+
+            var expectedUserValidationException = new UserValidationException(invalidUserException);
+
+            //when
+            ValueTask<User> addUserTask = this.userService.AddUserAsync(invalidUser);
+
+            UserValidationException userValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(addUserTask.AsTask);
+
+            userValidationException.Should().BeEquivalentTo(expectedUserValidationException);
+
+            //then
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SamExceptionAs(expectedUserValidationException))),
+                Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(It.IsAny<User>()),
+                Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ Sheenam.MVC/Models/Users/Exceptions/InvalidUserException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace Sheenam.MVC.Models.Users.Exceptions
+{
+    public class InvalidUserException : Xeption
+    {
+        public InvalidUserException()
+            :base("User is invalid")
+        { }
+
+    }
+}
